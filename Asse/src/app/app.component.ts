848685fe586ecb36a641c7010f5c32eafa340e69
DaniloJovanovicGIT <dj20180130@student.fon.bg.ac.rs>
2024-07-23 15:06:20 +0200

    Implemented financial overview componenet

--- Asse/src/app/app.component.ts
+++ Asse/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { FinancialOverviewComponent } from "./financial-overview/financial-overview.component";
+import { FiltersComponent } from './filters/filters.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterOutlet, NavigationComponent, ToolbarComponent, FinancialOverviewComponent,FiltersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ Asse/src/app/financial-overview/financial-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-financial-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './financial-overview.component.html',
+  styleUrl: './financial-overview.component.scss'
+})
+export class FinancialOverviewComponent {
+
+}
